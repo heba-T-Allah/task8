5a968900a2ea55a59aea1c93adbcc6cb3b696bf7
heba <beboozain7@gmail.com>
2023-10-02 01:03:16 +0300

    shadow button added

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,31 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Stack(children: [MyBody(),
-          Align(
-          alignment: FractionalOffset.bottomCenter,
-          child: MaterialButton(onPressed: () => {}, child: Container(height: 3,width: 100,color: Colors.black,)),
-        )],),
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Stack(
+            children: [
+              MyBody(),
+              Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: MaterialButton(
+                    onPressed: () => {},
+                    child: Container(
+                      height: 3,
+                      width: 100,
+                      color: Colors.black,
+                    )),
+              )
+            ],
+          ),
+        ));
   }
 }
+
 class MyBody extends StatefulWidget {
   const MyBody({super.key});
 
@@ -42,85 +52,91 @@ class _MyBodyState extends State<MyBody> {
 
   @override
   Widget build(BuildContext context) {
-    return  ListView(children: [
-        const SizedBox(height: 50),
-        Center(
-          // child: Icon(Icons.diamond_outlined,),
-            child: SizedBox.fromSize(
-              size: const Size.fromRadius(40),
-              child: const FittedBox(
-                child: Icon(Icons.diamond_outlined),
-              ),
-            )),
-        const Padding(
-          padding: EdgeInsets.only(top: 20.0),
-          child: Center(
-              child: Text(
-                "SHRINE",
-                style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),
-              )),
+    return ListView(children: [
+      const SizedBox(height: 50),
+      Center(
+          child: SizedBox.fromSize(
+        size: const Size.fromRadius(40),
+        child: const FittedBox(
+          child: Icon(Icons.diamond_outlined),
         ),
-        const SizedBox(height: 80),
-        Padding(
-          padding: const EdgeInsets.only(
-              top: 25.0, right: 25, left: 25, bottom: 15),
-          child: TextField(
-            controller: _userNameController,
-            decoration: const InputDecoration(
-              filled: true,
-              contentPadding: EdgeInsets.all(20),
-              hintText: "User name",
-              fillColor: Color(0xfff1f3f4),
+      )),
+      const Padding(
+        padding: EdgeInsets.only(top: 20.0),
+        child: Center(
+            child: Text(
+          "SHRINE",
+          style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),
+        )),
+      ),
+      const SizedBox(height: 80),
+      Padding(
+        padding:
+            const EdgeInsets.only(top: 25.0, right: 25, left: 25, bottom: 15),
+        child: TextField(
+          controller: _userNameController,
+          decoration: const InputDecoration(
+            filled: true,
+            contentPadding: EdgeInsets.all(20),
+            hintText: "User name",
+            fillColor: Color(0xfff1f3f4),
+          ),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(right: 25, left: 25),
+        child: TextField(
+          controller: _passwordController,
+          decoration: const InputDecoration(
+            filled: true,
+            hintText: "Password",
+            contentPadding: EdgeInsets.all(20),
+            fillColor: Color(0xfff1f3f4),
+          ),
+          obscureText: true,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: OverflowBar(
+          alignment: MainAxisAlignment.end,
+          children: [
+            TextButton(
+              onPressed: () {
+                _userNameController.clear();
+                _passwordController.clear();
+              },
+              child: const Text("CANCEL"),
             ),
-          ),
+            Container(
+                width: 90,
+                height: 60,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(70),
+                    color: const Color(0xfff2f2f2),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        // spreadRadius: 1,
+                        blurRadius:3,
+                        offset: Offset(0,4),
+                      )
+                    ]),
+                child: MaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      userName = _userNameController.text;
+                      password = _passwordController.text;
+                      print(
+                          "Your UserName is $userName and your password is $password");
+                    });
+                  },
+                  textColor: Colors.blue,
+                  child: const Text("NEXT"),
+                )),
+          ],
         ),
-        Padding(
-          padding: const EdgeInsets.only(right: 25, left: 25),
-          child: TextField(
-            controller: _passwordController,
-            decoration: const InputDecoration(
-              filled: true,
-              hintText: "Password",
-              contentPadding: EdgeInsets.all(20),
-              fillColor: Color(0xfff1f3f4),
-            ),
-            obscureText: true,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: OverflowBar(
-            alignment: MainAxisAlignment.end,
-            children: [
-              TextButton(
-                onPressed: () {
-                  _userNameController.clear();
-                  _passwordController.clear();
-                },
-                child: const Text("CANCEL"),
-              ),
-              Container(
-                  width: 90,
-                  height: 60,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(70),
-                      color: const Color(0xfff2f2f2)),
-                  child: MaterialButton(
-                    onPressed: () {
-                      setState(() {
-                        userName = _userNameController.text;
-                        password = _passwordController.text;
-                        print(
-                            "Your UserName is $userName and your password is $password");
-                      });
-                    },
-                    textColor: Colors.blue,
-                    child: const Text("NEXT"),
-                  )),
-            ],
-          ),
-        ),
-
-      ]);
+      ),
+    ]);
   }
 }
